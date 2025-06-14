b4e35da67a916bdc1af53301b00925e0f0963b1b
okoroaforchigozie <okoroaforc14@gmail.com>
2025-06-14 14:24:21 -0200

    update: added allowed hosts

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ const { createAdmin } = require("./src/db/querys/admin")
 
 const app = express()
 
-app.use(cors())
+app.use(cors(
+    {
+        origin: ["http://localhost:5173", "http://localhost:3000", "http://localhost:3001", "https://rags-and-soles.netlify.app"]
+    }
+))
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
@@ -34,14 +38,15 @@ app.use(errorHandler)
 const port = process.env.PORT ?? 9500
 
 
-console.log("proposed port::",port)
+console.log("proposed port::", port)
 createDatabaseIfNotExists().then(() => {
     sync().then(() => {
-        createAdmin().then(() =>{
+        createAdmin().then(() => {
             app.listen(port, () => {
                 console.log("running:::", port)
-            })}
-        ).catch((error) =>{
+            })
+        }
+        ).catch((error) => {
             console.log("unable to create admin")
             console.log(error)
         })
