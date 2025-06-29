48e01c33964391765fb6e7c2800655493aed2970
chigozie <okoroaforc14@gmail.com>
2025-06-29 21:11:26 +0100

    update: removed files from sync

--- src/db/sync.js
+++ src/db/sync.js
@@ -18,20 +18,20 @@ exports.sync = async () => {
     conn.authenticate().then(async () => {
         await Promise.allSettled(
             [
-                user.sync({ alter: true }), // merge to main
-                admin.sync({ alter: true }),
-                cart.sync({ alter: true }), //sync with main
-                category.sync({ alter: true }),
-                images.sync({alter:true}),
-                order.sync({alter:true}),
-                product.sync({alter:true}),
-                review.sync({alter:true}),
-                shipping.sync({alter:true}),
-                transaction.sync({alter:true}),
-                coupon.sync({alter:true}),
-                deliv_locations.sync({alter:true}),
-                specifications.sync({alter: true}),
-                ordersOnly.sync({alter:true})
+                // user.sync({ alter: true }), // merge to main
+                // admin.sync({ alter: true }),
+                // cart.sync({ alter: true }), //sync with main
+                // category.sync({ alter: true }),
+                // images.sync({alter:true}),
+                // order.sync({alter:true}),
+                // product.sync({alter:true}),
+                // review.sync({alter:true}),
+                // shipping.sync({alter:true}),
+                // transaction.sync({alter:true}),
+                // coupon.sync({alter:true}),
+                // deliv_locations.sync({alter:true}),
+                // specifications.sync({alter: true}),
+                // ordersOnly.sync({alter:true})
             ]
         )
     })
